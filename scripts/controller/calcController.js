c1e86aea82d42c9fef38ec061f32c29a789b02c6
gferreira <gferreiraa@gmail.com>
2019-01-27 16:33:04 -0200

    Created: Controller

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,24 @@
 class CalcController {
-    
+    constructor(){ // Chamado automaticamente junto a instância de uma classe
+        this._displayCalc = 0; // Por convensão _nome refere-se a um atribuito privado.
+        this._dataAtual;
+    }
+    // Retorna uma valor.
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    // Atribui um valor através de um parâmetro de entrada.
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
+
+
 }
