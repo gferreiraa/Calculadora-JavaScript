c217c7768602eb0864fd8fd563934c709bf08a38
gferreira <gferreiraa@gmail.com>
2019-02-04 23:57:39 -0200

    DOM

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -4,23 +4,36 @@ class CalcController {
 
         this._displayCalc = "0";
         this._dataAtual;
+        this.initialize();
 
     }
-    // get and set para display
-    get displayCalc(){
+    
+    initialize(){
+
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
+
+        displayCalcEl.innerHTML = '4567';
+        dateEl.innerHTML = '10/12/2099';
+        timeEl.innerHTML = '23:59'; // propriedade que inseri um 'valor' no HTML
+
+    }
+
+    get displayCalc(){           // get and set para display
         return this._displayCalc;
     }
 
     set displayCalc(valor){ 
         this._displayCalc = valor
     }
-    // get and set para data
-    get dataAtual(){
+    
+    get dataAtual(){           // get and set para data
         return this._dataAtual;
     }
 
     set dataAtual(valor){
-        this.dataAtual = valor;
+        this._dataAtual = valor;
     }
 
 }
