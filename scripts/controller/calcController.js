3090fc76c8d72e53901b2efa8a6e75cdd80c37b4
gferreira <gferreiraa@gmail.com>
2019-02-04 23:31:29 -0200

    Created: Getters and Setters

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,26 @@
 class CalcController {
 
+    constructor() {
+
+        this._displayCalc = "0";
+        this._dataAtual;
+
+    }
+    // get and set para display
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){ 
+        this._displayCalc = valor
+    }
+    // get and set para data
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        this.dataAtual = valor;
+    }
+
 }
