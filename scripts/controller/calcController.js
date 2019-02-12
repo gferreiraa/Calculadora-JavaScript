605232bacc9bcecad97007c605abae08f9a604e1
gferreira <gferreiraa@gmail.com>
2019-02-11 22:22:10 -0200

    Feature: Percent

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -23,6 +23,8 @@ class CalcController {
 
         }, 1000);
 
+        this.setLastNumberToDisplay();
+
     }
 
     addEventListenerAll(element, events, fn){
@@ -39,11 +41,15 @@ class CalcController {
 
         this._operation = [];
 
+        this.setLastNumberToDisplay();
+
     }
 
     clearEntry() {
 
         this._operation.pop();
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -79,11 +85,25 @@ class CalcController {
 
     calc(){
 
-        let last = this._operation.pop();
+        let last = '';
+
+        if (this._operation.length > 3) {
+            last = this._operation.pop();
+        }
 
         let result = eval(this._operation.join(''));
 
-        this._operation = [result, last];
+        if(last == '%') {
+
+            result /= 100;
+
+            this._operation = [result];
+
+        } else {
+
+            this._operation = [result];
+
+        }
 
         this.setLastNumberToDisplay();
 
@@ -101,6 +121,8 @@ class CalcController {
             }
 
         }
+
+        if(!lastNumber) lastNumber = 0;
 
         this.displayCalc = lastNumber;
     
@@ -177,7 +199,7 @@ class CalcController {
                 this.addOperation('%');     
                 break;
             case  'igual':
-                    
+                    this.calc();
                 break;
 
             case 'ponto':
