a7f2f40077021eef48bc714ab9d222c4ddeed0c7
gferreira <gferreiraa@gmail.com>
2019-02-13 00:36:31 -0200

    Feature: Dot

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -43,6 +43,8 @@ class CalcController {
     clearAll() {
 
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
 
         this.setLastNumberToDisplay();
 
@@ -177,10 +179,6 @@ class CalcController {
 
                 this.setLastOperation(value);
 
-            } else if(isNaN(value)) {
-
-                console.log('Outra coisa', value);
-
             } else {
                 
                 this.pushOperation(value);
@@ -198,7 +196,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
 
@@ -211,6 +209,20 @@ class CalcController {
     setError() {
 
         this.displayCalc = 'Error';
+
+    }
+
+    addDot() {
+
+        let lastOperation = this.getLastOperation();
+
+        if (this.isOperator(lastOperation) || !lastOperation) {
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -244,7 +256,7 @@ class CalcController {
                 break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot();
                 break;
              
             case '0':
