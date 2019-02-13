62d515e1be9e011baab25a61e6d1d4a35d9d58bd
gferreira <gferreiraa@gmail.com>
2019-02-13 00:47:25 -0200

    Update: Dot

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -196,7 +196,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
 
@@ -213,8 +213,10 @@ class CalcController {
     }
 
     addDot() {
-
+        
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
